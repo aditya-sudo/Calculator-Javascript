83f0118c4e107987f3f87b34e737fa35b86667e4
aditya-sudo <adityashah89156@gmail.com>
2022-01-05 18:35:08 +0530

    calculation operations added

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-function add(num1, num2) {
-  return num1 + num2;
+function print(x) {
+  var y = document.getElementById(x).innerHTML;
+  document.getElementById("display").innerHTML += y;
 }
-function sub(num1, num2) {
-  return num1 - num2;
+function answer(x) {
+  let y = document.getElementById(x).innerHTML.toString();
+
+  document.getElementById("display").innerHTML = eval(y);
 }
-function mul(num1, num2) {
-  return num1 * num2;
+function clearscreen() {
+  document.getElementById("display").innerHTML = "";
 }
-function div(num1, num2) {
-  return num1 / num2;
+function square(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = a * a;
+  document.getElementById("display").innerHTML = b;
 }
-function square(num) {
-  return num * num;
+function absolute(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.abs(a);
+  document.getElementById("display").innerHTML = b;
+}
+function expo(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.exp(a);
+  document.getElementById("display").innerHTML = b;
+}
+function naturallog(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.log(a);
+  document.getElementById("display").innerHTML = b;
+}
+function log(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.log10(a);
+  document.getElementById("display").innerHTML = b;
+}
+function pi() {
+  let b = Math.PI;
+  document.getElementById("display").innerHTML = b;
+}
+function root(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.sqrt(a);
+  document.getElementById("display").innerHTML = b;
+}
+function power10(x) {
+  let a = document.getElementById(x).innerHTML;
+  let b = Math.pow(10, a);
+  document.getElementById("display").innerHTML = b;
+}
+function fact(x) {
+  var i, num, f;
+  f = 1;
+  num = document.getElementById(x).innerHTML;
+  for (i = 1; i <= num; i++) {
+    f = f * i;
+  }
+  i = i - 1;
+  document.getElementById("display").innerHTML = f;
+}
+function del(x) {
+  let a = document.getElementById(x).innerHTML.toString();
+  let b = a.slice(0, -1);
+  document.getElementById("display").innerHTML = b;
 }
